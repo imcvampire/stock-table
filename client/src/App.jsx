6ed184f59b0b7b6441e06bfffd1e605e22f98a5b
imcvampire <quocanh96@gmail.com>
2017-09-15 17:38:14 +0700

    feat(App): add Route + Link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import StockTable from './components/StockTable'
 
@@ -7,7 +7,46 @@ const App = () => (
   <BrowserRouter>
     <div>
       <Navbar />
-      <StockTable stockList={[]} />
+
+      <Switch>
+        <Route
+          exact
+          path="/gainer/top"
+          render={() => (
+            <StockTable
+              stockList={[
+                {
+                  code: 'ABC.XY',
+                  company: 'NQA JSC',
+                  price: 55.11,
+                  number: 1000,
+                  change: 2,
+                  percentage: 6,
+                },
+              ]}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/loser/top"
+          render={() => (
+            <StockTable
+              stockList={[
+                {
+                  code: 'DEF.MN',
+                  company: 'Quant Edge',
+                  price: 23.45,
+                  number: 100001,
+                  change: 2.1,
+                  percentage: 6,
+                },
+              ]}
+            />
+          )}
+        />
+        <Redirect to="/gainer/top" />
+      </Switch>
     </div>
   </BrowserRouter>
 )
--- client/src/components/Navbar/NavbarTabs.jsx
+++ client/src/components/Navbar/NavbarTabs.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Tabs, Tab } from 'material-ui/Tabs'
+import { Link } from 'react-router-dom'
 
 const styles = {
   tabs: {
@@ -11,11 +12,13 @@ const NavbarTabs = () => (
   <Tabs style={styles.tabs}>
     <Tab
       label="Top gainers"
-      data-route="/gainers/top"
+      data-route="/gainer/top"
+      containerElement={<Link to="/gainer/top" />}
     />
     <Tab
       label="Top losers"
-      data-route="/losers/top"
+      data-route="/loser/top"
+      containerElement={<Link to="/loser/top" />}
     />
   </Tabs>
 )
