9bf1a24d8b5d1af71c0707a489d6c8d8d05e6be7
imcvampire <quocanh96@gmail.com>
2017-09-17 11:43:25 +0700

    fix: typo

--- server/app/utils/getRandomNumber.js
+++ server/app/utils/getRandomNumber.js
@@ -2,7 +2,7 @@
  * Get random number between min and max
  * @param {Number} min
  * @param {Number} max
- * @return {Number} 
+ * @return {Number}
  */
 const getRandomNumber = (min, max) => Math.random() * (max - min) + min
 
