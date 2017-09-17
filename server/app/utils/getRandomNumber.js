0ebd7159e684b568365c3fe6221cc46091fdfa8c
imcvampire <quocanh96@gmail.com>
2017-09-17 11:42:20 +0700

    feat(server): update jsdoc

--- server/app/utils/getRandomNumber.js
+++ server/app/utils/getRandomNumber.js
@@ -2,6 +2,7 @@
  * Get random number between min and max
  * @param {Number} min
  * @param {Number} max
+ * @return {Number} 
  */
 const getRandomNumber = (min, max) => Math.random() * (max - min) + min
 
